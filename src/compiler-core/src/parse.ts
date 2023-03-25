9934611f207a845d95fbb251308f59b93c990096
hws <286572050@qq.com>
2023-03-25 19:04:14 +0800

    解析element类型

--- src/compiler-core/src/parse.ts
+++ src/compiler-core/src/parse.ts
@@ -1,5 +1,9 @@
 import { NodeTypes } from "./ast";
 
+const enum TagType {
+  Start,
+  End,
+}
 export function baseParse(content: string) {
   const context = createParseContext(content);
   return createRoot(parseChildren(context));
@@ -7,8 +11,16 @@ export function baseParse(content: string) {
 function parseChildren(context: any) {
   const nodes = [];
   let node;
-  if (context.source.startsWith("{{")) {
+  const s = context.source;
+  // 插值类型
+  if (s.startsWith("{{")) {
     node = parseInterpolation(context);
+  }
+  // ELEMENT
+  else if (s[0] === "<") {
+    if (/[a-z]/i.test(s[1])) {
+      node = parseElement(context);
+    }
   }
   nodes.push(node);
   return nodes;
@@ -53,4 +65,23 @@ function createRoot(children: any) {
 }
 function createParseContext(content: any) {
   return { source: content };
+}
+function parseElement(context: any) {
+  const element = parseTag(context, TagType.Start);
+  parseTag(context, TagType.End);
+  return element;
+}
+function parseTag(context: any, type: TagType) {
+  const match: any = /^<\/?([a-z]*)/i.exec(context.source);
+  const tag = match[1];
+
+  advanceBy(context, match[0].length);
+  advanceBy(context, 1);
+  if (type === TagType.End) {
+    return;
+  }
+  return {
+    type: NodeTypes.ELEMENT,
+    tag,
+  };
 }
